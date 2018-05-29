ac652550c9e1fed2d999db4e65992c963159ea60
Abhishek Sarkar <kelsnare@gmail.com>
2018-05-29 13:04:22 +0530

    Initial Commit

---
+++ installer.sh
@@ -0,0 +1,19 @@
+wget https://dl.pstmn.io/download/latest/linux64 -O postman.tar.gz
+sudo tar -xzf postman.tar.gz -C /opt
+rm postman.tar.gz
+echo "\n*******************\nInstalling Binary\n********************\n"
+sudo ln -s "/opt/Postman/Postman" "/usr/bin/postman"
+echo "\n*******************\nCreating Desktop App\n**********************\n"
+locVar=`find "/opt/Postman" -name "icon.png"`
+cat > /tmp/postman.desktop <<EOL
+[Desktop Entry]
+Encoding=UTF-8
+Name=Postman
+Exec=postman
+Icon=$locVar
+Terminal=false
+Type=Application
+Categories=Development;
+EOL
+sudo desktop-file-install --dir="/usr/share/applications" --delete-original /tmp/postman.desktop
+echo "Done\n***********************\n"
